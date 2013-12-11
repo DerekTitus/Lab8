c2f66c8ea4f20eadb8b03aaa1b9b70dccd9037f2
Derek Titus <c15derek.titus@usafa.edu>
2013-12-11 15:29:57 -0700

    has A functionality

--- main.c
+++ main.c
@@ -1,10 +1,52 @@
 #include <msp430.h> 
+#include "MoveBot.h"
+#include "RobotSensors.h"
 
 /*
  * main.c
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
+//    P1DIR |= BIT0|BIT6;
+//    P1OUT &= ~(BIT0|BIT6);
+    initSensors();
+    initMotors();
+
+
+    while(1)
+        {
+
+        	initLeftSensor();
+        	if (initLeftSensor() >= 0x250){
+        		MoveSmallRight();
+        	}
+        	else{
+        		MoveForward();
+        	}
+        	initLeftSensor();
+        	if (initLeftSensor() <= 0x270){
+        		MoveSmallLeft();
+        	}
+        	else{
+        		MoveForward();
+        	}
+
+        	initCenterSensor();
+        	if (initCenterSensor() >= 0x300){
+        		StopBot();
+        		ShortDelay();
+        		TurnRight();
+        		ShortDelay();
+        	}
+        	else{
+        		MoveForward();
+        	}
+        	}
+
+
+
+
+
+
 	return 0;
 }
